7141d616a969e80b413098163a95c16811a2fa22
abunaem123 <88497497+abunaem123@users.noreply.github.com>
2022-02-17 15:01:40 +0600

    error first

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function getIncome() {
+function incomeTotal() {
     const incomeField = document.getElementById('income-input');
     let getIncomeField = parseFloat(incomeField.value);
     return getIncomeField;
@@ -6,19 +6,26 @@ function getIncome() {
 
 function catchRemainingBalance() {
     const foodField = document.getElementById('food-input');
-    const getFoodField = foodField.value;
+    const getFoodField = parseFloat(foodField.value);
     const rentField = document.getElementById('rent-input');
-    const getRentField = rentField.value;
+    const getRentField = parseFloat(rentField.value);
     const clothesField = document.getElementById('clothes-input');
-    const getClothesField = clothesField.value;
+    const getClothesField = parseFloat(clothesField.value);
+    //error massage
+    const errorMsg = document.getElementById('error-msg');
+    if (getFoodField < 0 || getRentField < 0 || getClothesField < 0) {
+        errorMsg.style.display = 'block';
+    } else {
+        errorMsg.style.display = 'none';
+    }
 
     //Calculate Expense Amount
     const expensesAmount = document.getElementById('expenses-amount');
-    expensesAmount.innerText = parseFloat(getFoodField) + parseFloat(getRentField) + parseFloat(getClothesField);
+    expensesAmount.innerText = getFoodField + getRentField + getClothesField;
 
     //Calculate Balance Amount
     const remainingBalance = document.getElementById('last-balance');
-    remainingBalance.innerText = getIncome() - expensesAmount.innerText;
+    remainingBalance.innerText = incomeTotal() - expensesAmount.innerText;
     return remainingBalance.innerText;
 }
 
@@ -29,13 +36,16 @@ document.getElementById('calculate-button').addEventListener('click', function (
 
 //Add Click Handler to save button
 document.getElementById('save-button').addEventListener('click', function () {
-    const saveField = document.getElementById('save-input');
-    // const getSaveField = saveField.value;
+    const saveInput = document.getElementById('save-input');
+    // const getSaveField = saveInput.value;
     const savingAmmount = document.getElementById('saving-amount');
     // let savingAmmountText = savingAmmount.innerText;
-    savingAmmount.innerText = (saveField.value / 100) * getIncome();
+    savingAmmount.innerText = (saveInput.value / 100) * incomeTotal();
     //Calculate Remaining Balance
     const remainingBalance = document.getElementById('remaining-balance');
+
     remainingBalance.innerText = catchRemainingBalance() - savingAmmount.innerText;
     console.log(remainingBalance.innerText);
+
+    // error massage
 });
